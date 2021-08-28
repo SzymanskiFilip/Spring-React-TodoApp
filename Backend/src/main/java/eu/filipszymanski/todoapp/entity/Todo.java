ce53c5a370fa80cd8f86a23424845a2b44d509d1
Filip <szymanski.fp@gmail.com>
2021-08-28 18:03:56 +0200

    created todo entity class

---
+++ Backend/src/main/java/eu/filipszymanski/todoapp/entity/Todo.java
@@ -0,0 +1,19 @@
+package eu.filipszymanski.todoapp.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String todo;
+    private String creatorName;
+
+
+}
