0e7ac7bad337926d3274327b75aa3087a4dbbd38
Filip <szymanski.fp@gmail.com>
2021-08-29 20:13:12 +0200

    created confirmation component

---
+++ Frontend/todoapp/src/components/Confirmation.js
@@ -0,0 +1,13 @@
+function Confirmation() {
+  return (
+    <div className="confirmation">
+      <div className="modal">
+        <p>Are you sure?</p>
+        <button className="btn">Cancel</button>
+        <button className="btn">Confirm</button>
+      </div>
+    </div>
+  );
+}
+
+export default Confirmation;
