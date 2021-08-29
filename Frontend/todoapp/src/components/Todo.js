e533e4e3b7fc0581f1850fc6be0f861e526f44d9
Filip <szymanski.fp@gmail.com>
2021-08-29 20:36:57 +0200

    moved confirmation to Todo component

--- Frontend/todoapp/src/components/Todo.js
+++ Frontend/todoapp/src/components/Todo.js
@@ -1,11 +1,21 @@
+import React, { useState } from "react";
+import Confirmation from "./Confirmation.js";
+
 function Todo(props) {
+  const [confirmationIsOpen, setConfirmationIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setConfirmationIsOpen(true);
+  }
+
   return (
     <div className="card">
       <h1>{props.title}</h1>
       <h2>{props.creator}</h2>
       <div className="flex">
-        <button>delete</button>
+        <button onClick={deleteHandler}>delete</button>
       </div>
+      {confirmationIsOpen && <Confirmation />}
     </div>
   );
 }
