7e01fac30cf59bd57d62dd2a5ad652219f944b6c
Filip <szymanski.fp@gmail.com>
2021-08-29 20:17:57 +0200

    conditional render of confirmation element

--- Frontend/todoapp/src/App.js
+++ Frontend/todoapp/src/App.js
@@ -1,14 +1,19 @@
+import React, { useState } from "react";
+
 import "./index.css";
 import Todo from "./components/Todo.js";
 import Confirmation from "./components/Confirmation.js";
+
 function App() {
+  const [confirmationIsOpen, setConfirmationIsOpen] = useState(false);
+
   return (
     <div>
       <h1 className="title">Todos</h1>
       <div className="container">
         <Todo title="Wash the car" creator="Filip" />
         <Todo title="Eat ice cream" creator="Filip" />
-        <Confirmation />
+        {confirmationIsOpen && <Confirmation />}
       </div>
     </div>
   );
