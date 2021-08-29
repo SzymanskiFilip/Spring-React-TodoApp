aa4e54d16f28cf788b317a1b8e9e3bba67ba6178
Filip <szymanski.fp@gmail.com>
2021-08-29 20:37:07 +0200

    moved confirmation to Todo component

--- Frontend/todoapp/src/App.js
+++ Frontend/todoapp/src/App.js
@@ -1,19 +1,13 @@
-import React, { useState } from "react";
-
 import "./index.css";
 import Todo from "./components/Todo.js";
-import Confirmation from "./components/Confirmation.js";
 
 function App() {
-  const [confirmationIsOpen, setConfirmationIsOpen] = useState(false);
-
   return (
     <div>
       <h1 className="title">Todos</h1>
       <div className="container">
         <Todo title="Wash the car" creator="Filip" />
         <Todo title="Eat ice cream" creator="Filip" />
-        {confirmationIsOpen && <Confirmation />}
       </div>
     </div>
   );
