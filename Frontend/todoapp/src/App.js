4216ce15b32cb3cfb2668fa7bde6109d2bedd1c5
Filip <szymanski.fp@gmail.com>
2021-08-29 19:56:04 +0200

    added dummy cards and finished layout

--- Frontend/todoapp/src/App.js
+++ Frontend/todoapp/src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import './index.css';
-import './TodoForm.js';
-import TodoForm from './TodoForm.js';
-
+import "./index.css";
 function App() {
   return (
     <div>
-      <TodoForm />
+      <h1 className="title">Todos</h1>
+      <div className="container">
+        <div className="card">
+          <h1>Wash the car</h1>
+          <h2>Filip</h2>
+          <div className="flex">
+            <button>delete</button>
+          </div>
+        </div>
+
+        <div className="card">
+          <h1>Wash the car</h1>
+          <h2>Filip</h2>
+          <div className="flex">
+            <button>delete</button>
+          </div>
+        </div>
+
+        <div className="card">
+          <h1>Wash the car</h1>
+          <h2>Filip</h2>
+          <div className="flex">
+            <button>delete</button>
+          </div>
+        </div>
+
+        <div className="card">
+          <h1>Wash the car</h1>
+          <h2>Filip</h2>
+          <div className="flex">
+            <button>delete</button>
+          </div>
+        </div>
+
+        <div className="card">
+          <h1>Wash the car</h1>
+          <h2>Filip</h2>
+          <div className="flex">
+            <button>delete</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
