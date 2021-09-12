17dd4e80e46b2d7ee7ba01ea9e61a2e873db0fd5
Filip <szymanski.fp@gmail.com>
2021-09-12 21:01:43 +0200

    deleted whole backend

--- Backend/src/main/java/eu/filipszymanski/todoapp/repository/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package eu.filipszymanski.todoapp.repository;
-
-import eu.filipszymanski.todoapp.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-}
