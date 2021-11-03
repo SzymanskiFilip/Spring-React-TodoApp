58eb6fdc96fddf6d798dcf120fe4cea7bd1cb3b0
Filip <szymanski.fp@gmail.com>
2021-11-03 21:06:07 +0100

    added instance of repo interface

--- Backend/src/main/java/com/example/todoapp/controllers/TodoController.java
+++ Backend/src/main/java/com/example/todoapp/controllers/TodoController.java
@@ -1,10 +1,15 @@
 package com.example.todoapp.controllers;
 
+import com.example.todoapp.repositories.TodoRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class TodoController {
+    private final TodoRepository repository;
+    public TodoController(TodoRepository repository){
+        this.repository = repository;
+    }
 
 }
