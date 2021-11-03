4162cb42a0a62cb34fe01a6ebc82d5643f6a1340
Filip <szymanski.fp@gmail.com>
2021-11-03 21:32:53 +0100

    initial todolist

---
+++ Frontend/todo-app/src/Components/TodoList.js
@@ -0,0 +1,12 @@
+function TodoList(){
+    return(
+      <div className="todo-list">
+        <div className="todo">
+            <p>Train</p>
+            <input type="checkbox"/>
+        </div>
+      </div>
+    );
+}
+
+export default TodoList;
