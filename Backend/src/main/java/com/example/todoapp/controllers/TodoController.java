d80a7eeddd4d8032e14886e4767141e4892548d6
Filip <szymanski.fp@gmail.com>
2021-11-03 21:12:19 +0100

    added allTodos mapping

--- Backend/src/main/java/com/example/todoapp/controllers/TodoController.java
+++ Backend/src/main/java/com/example/todoapp/controllers/TodoController.java
@@ -1,8 +1,12 @@
 package com.example.todoapp.controllers;
 
+import com.example.todoapp.entities.Todo;
 import com.example.todoapp.repositories.TodoRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -12,4 +16,9 @@ public class TodoController {
         this.repository = repository;
     }
 
+
+    @GetMapping("/todos")
+    List<Todo> allTodos(){
+        return repository.findAll();
+    }
 }
