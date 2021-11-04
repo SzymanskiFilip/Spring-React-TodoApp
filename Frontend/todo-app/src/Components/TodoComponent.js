4bdf5fdb27a583c455cd4421a828330960270e6f
Filip <szymanski.fp@gmail.com>
2021-11-04 21:11:51 +0100

    fixed default checked

--- Frontend/todo-app/src/Components/TodoComponent.js
+++ Frontend/todo-app/src/Components/TodoComponent.js
@@ -1,8 +1,22 @@
+import {useState} from "react";
+
 function TodoComponent(props){
+
+    const [status, setStatus] = useState(props.status);
+
+    function handleCheck(){
+        console.log(status)
+        if(status === true){
+            setStatus(false);
+        } else{
+            setStatus(true);
+        }
+    }
+
     return(
         <div className="todo">
-            <input type="checkbox"/>
-            <p>{props.title}</p>
+            <input type="checkbox" defaultChecked={status} onChange={handleCheck}/>
+            <p className={status ? 'done' : 'undone'}>{props.title}</p>
         </div>
     );
 }
