53d8c78037867841d8dd10fb9a52cb20cbcb9ff5
Filip <szymanski.fp@gmail.com>
2021-11-04 20:45:41 +0100

    mapping through pulled todos

--- Frontend/todo-app/src/Components/TodoList.js
+++ Frontend/todo-app/src/Components/TodoList.js
@@ -3,19 +3,26 @@ import TodoComponent from "./TodoComponent";
 
 function TodoList(){
 
+    const [todos, setTodos] = useState([]);
+
     async function getTodos(){
         const res = await fetch("api/v1/todos");
         const json = await res.json();
         console.log(json);
+        setTodos(json);
     }
 
-
+    useEffect(() => {
+        getTodos();
+    },[]);
 
     return(
       <div className="todo-list">
-          <TodoComponent title="Train"/>
-          <TodoComponent title="Go Shopping"/>
-          <TodoComponent title="Study for maths exam"/>
+          {
+              todos.map((todo) => (
+                <TodoComponent title={todo.title}/>
+              ))
+          }
       </div>
     );
 }
