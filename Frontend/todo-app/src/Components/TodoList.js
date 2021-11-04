66dcd5e730f6c185b163281c2fa361e670f0a291
Filip <szymanski.fp@gmail.com>
2021-11-04 21:47:45 +0100

    passing id as prop

--- Frontend/todo-app/src/Components/TodoList.js
+++ Frontend/todo-app/src/Components/TodoList.js
@@ -20,7 +20,7 @@ function TodoList(){
       <div className="todo-list">
           {
               todos.map((todo) => (
-                <TodoComponent title={todo.title} status={todo.status} key={todo.id}/>
+                <TodoComponent title={todo.title} status={todo.status} key={todo.id} id={todo.id}/>
               ))
           }
       </div>
