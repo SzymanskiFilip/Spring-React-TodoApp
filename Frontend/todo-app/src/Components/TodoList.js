544f20bb520ccca0a22482e2fdd2715fdb4ce6bb
Filip <szymanski.fp@gmail.com>
2021-11-04 20:39:20 +0100

    added 3 todos and a async getTodos() function

--- Frontend/todo-app/src/Components/TodoList.js
+++ Frontend/todo-app/src/Components/TodoList.js
@@ -1,10 +1,21 @@
+import {useState, useEffect} from "react";
+import TodoComponent from "./TodoComponent";
+
 function TodoList(){
+
+    async function getTodos(){
+        const res = await fetch("api/v1/todos");
+        const json = await res.json();
+        console.log(json);
+    }
+
+
+
     return(
       <div className="todo-list">
-        <div className="todo">
-            <p>Train</p>
-            <input type="checkbox"/>
-        </div>
+          <TodoComponent title="Train"/>
+          <TodoComponent title="Go Shopping"/>
+          <TodoComponent title="Study for maths exam"/>
       </div>
     );
 }
