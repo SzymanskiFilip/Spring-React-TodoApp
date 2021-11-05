61818588fa87328bef51aee7804ad14895381a62
Filip <szymanski.fp@gmail.com>
2021-11-05 23:51:43 +0100

    created a box to confirm delete

--- Frontend/todo-app/src/Components/ConfirmDeleting.js
+++ Frontend/todo-app/src/Components/ConfirmDeleting.js
@@ -1,6 +1,15 @@
 function ConfirmDeleting(){
     return(
-        <h1></h1>
+        <div className="confirm-prompt">
+            <h1>Are you sure you want to delete</h1>
+            <h1>Work out?</h1>
+
+            <div>
+                <button>Yes</button>
+                <button>Cancel</button>
+             </div>
+        </div>
+
     )
 }
 
