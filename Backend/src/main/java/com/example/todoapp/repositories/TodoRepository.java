a22a41581e7a34912f3f9da4658fabe796f3eddd
Filip <szymanski.fp@gmail.com>
2021-11-06 23:41:32 +0100

    added custom updateTodo method with a custom query but not using it

--- Backend/src/main/java/com/example/todoapp/repositories/TodoRepository.java
+++ Backend/src/main/java/com/example/todoapp/repositories/TodoRepository.java
@@ -1,10 +1,14 @@
 package com.example.todoapp.repositories;
 
 import com.example.todoapp.entities.Todo;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface TodoRepository extends CrudRepository<Todo, Long> {
     List<Todo> findAll();
+
+    @Query(value = "UPDATE todos SET status = ?1 WHERE id = ?2", nativeQuery = true)
+    Todo updateTodo(Boolean status, Integer id);
 }
