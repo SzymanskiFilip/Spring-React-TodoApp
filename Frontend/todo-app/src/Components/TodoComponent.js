ec150ae1acf73c24c76f3a86cbcf0ba3d4e2d7dc
Filip <szymanski.fp@gmail.com>
2021-11-06 23:09:26 +0100

    passing status and id to updateChecked of parent

--- Frontend/todo-app/src/Components/TodoComponent.js
+++ Frontend/todo-app/src/Components/TodoComponent.js
@@ -11,7 +11,7 @@ function TodoComponent(props){
         } else{
             setStatus(true);
         }
-        props.updateChecked(status);
+        props.updateChecked(status, props.id);
     }
 
     function showPrompt(){
