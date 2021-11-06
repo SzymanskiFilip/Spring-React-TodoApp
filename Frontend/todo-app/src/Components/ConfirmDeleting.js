15452cc1d96fff5b6d6591f0252f95af947d3ce6
Filip <szymanski.fp@gmail.com>
2021-11-06 22:39:26 +0100

    getting title from props

--- Frontend/todo-app/src/Components/ConfirmDeleting.js
+++ Frontend/todo-app/src/Components/ConfirmDeleting.js
@@ -1,12 +1,12 @@
-function ConfirmDeleting(){
+function ConfirmDeleting(props){
     return(
         <div className="confirm-prompt">
             <h1>Are you sure you want to delete</h1>
-            <h1>Work out?</h1>
+            <h1>{props.title}</h1>
 
-            <div>
-                <button>Yes</button>
-                <button>Cancel</button>
+            <div className="buttons-row">
+                <button className="btn">Yes</button>
+                <button className="btn">Cancel</button>
              </div>
         </div>
 
