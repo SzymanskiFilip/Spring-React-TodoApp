f75d7a4e7c5e0a577c0e337a5805f910bb3be83d
Filip <szymanski.fp@gmail.com>
2021-11-07 21:49:52 +0100

    created handleChange() that sets todo hook and dummy handleSubmit() that will change button state, post new todo and route back to main app

--- Frontend/todo-app/src/LoginPage/LoginPage.js
+++ Frontend/todo-app/src/LoginPage/LoginPage.js
@@ -1,11 +1,25 @@
+import {useState} from 'react';
+
 function LoginPage(){
+
+    const [todo, setTodo] = useState("");
+
+    function handleChange(event){
+        setTodo(event.target.value);
+    }
+
+    function handleSubmit(){
+        console.log(todo);
+    }
+
     return(
         <div className="form-container">
             <h1>Add your new todo!</h1>
             <form onSubmit={(e) => {
                 e.preventDefault();
+                handleSubmit();
             }}>
-                <input type="text" name="todo-title"/>
+                <input type="text" name="todo-title" onChange={handleChange}/>
                 <button>Submit</button>
             </form>
         </div>
